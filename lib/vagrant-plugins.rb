b00ac8c69574ac78160279f0387ec0f1a70caf56
Robert Schulze <robert@dotless.de>
2012-05-22 09:09:58 +0200

    defer loading of the plugin classes until vagrant's activation phase

--- lib/vagrant-plugins.rb
+++ lib/vagrant-plugins.rb
@@ -1,13 +1,14 @@
-require 'vagrant'
-require "vagrant-plugins/version"
-require "vagrant-plugins/helper"
-require "vagrant-plugins/ui"
-require "vagrant-plugins/command"
-
 module VagrantPlugininspection
   class Plugin < Vagrant.plugin("1")
     name "plugins"
     description "List all vagrant plugins loaded in the current vagrant environment"
+
+    activated do
+      require "vagrant-plugins/version"
+      require "vagrant-plugins/helper"
+      require "vagrant-plugins/ui"
+      require "vagrant-plugins/command"
+    end
 
     command('plugins') { Command }
   end
